ba7d7c2f7d2a9af021ac70b1e4b1fb958662d260
CindyConway <cindy@cindyconwaysair.att.net>
2014-07-10 21:21:47 -0700

    changed to MVC

--- application.js
+++ application.js
@@ -1,21 +1,61 @@
-$(document).ready(bindListeners);
+$(document).ready(function(){
+   var view = new View();
+   var model = new Model(); 
+   var controller = new Controller(view, model);
+   controller.bindListeners();
+});
 
-function bindListeners(){
- $('#roller button.add').on('click', addDice);
-  $('#roller button.roll').on('click', rollDice);
+function Controller(view, model){
+  this.view = view;
+  this.model = model;
 }
 
-  function addDice(){
-    console.log("WAT")
+Controller.prototype = {
+  bindListeners: function() {
+      $('#roller button.add').on('click', this.addDie.bind(this));
+      $('#roller button.roll').on('click', this.rollDice.bind(this));
+  },
+  addDie: function(){
+    this.view.addDie();
+  },
+  rollDice: function(){
+    var dieCount = this.view.dieCount
+    for (i=0; i < dieCount; i++){ 
+      this.view.updateDie(i, this.model.randomNumber())
+    }
+
+  }
+}
+
+
+function View(){
+    this.dieCount = 0;
+};
+
+View.prototype = { 
+  addDie: function(){
     $('.dice').append('<div class="die">0</div>');
+    this.dieCount = $('.die').length
+  },
+  updateDie: function(i, value){
+
+    $('.die:eq(' + i + ')').text(value)
+
   }
+}
 
-  function rollDice(){
+function Model(){
 
-    $('.die').each(randomNumber)
-  }
+};
 
-  function randomNumber(){
-      var value = Math.floor((Math.random()*6)+1);
-     $(this).text(value);
-   }
+Model.prototype = {
+  randomNumber: function () {
+      return Math.floor((Math.random()*6)+1);
+   },
+}
+
+
+
+
+  
+
